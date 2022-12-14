f23868875ccd3feaa1a525281d06e9d1c43284fb
Wilfred <wilfred05777@gmail.com>
2022-12-14 09:07:10 +0800

    0:6:13 Navbar and Footer Components initial-index

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <Head>
         <title>Pizza Restaurant in New York</title>
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       Homepage
-      {/* <Image src="/img/logo.png" width={500} height={500} alt="logo" /> */}
+      <Footer />
     </div>
   );
 }
