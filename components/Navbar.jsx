d8d87899a7fab5943e09bbae58a65f6ec281b0c2
Wilfred <wilfred05777@gmail.com>
2022-12-14 10:03:38 +0800

    0:21:24 Finished Navbar Styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
 import React from "react";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" width={32} height={32} alt="" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>12345 5132 22</div>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image
+            className={styles.logo} /* fixing the logo pixel */
+            src="/img/logo.png"
+            height={160}
+            width={69}
+            alt="logo"
+          />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" height={30} width={30} alt="logo" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
