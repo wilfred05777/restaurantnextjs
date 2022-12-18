ff626894b82f785446fd90dbbc6db7d2922e2754
Wilfred <wilfred05777@gmail.com>
2022-12-19 07:59:54 +0800

    1:00:29 Product styles.right working on left bug-

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Product.module.css";
 
 const Product = () => {
+  const [size, setSize] = useState(0);
+
   const pizza = {
     id: 1,
     img: "/img/pizza.png",
@@ -13,10 +15,31 @@ const Product = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <div className={styles.imgContainer}></div>
-        <Image src={pizza.img} layout="fill" alt="" />
+        <div className={styles.imgContainer}>
+          <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
+        </div>
       </div>
-      <div className="styles right"></div>
+
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>${pizza.price[0]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className="styles choose">Choose the size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src="/img/size.png" layout="fill" alt="" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
