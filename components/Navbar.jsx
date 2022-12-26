d31621f7812f5fc4b53340bf821b314abbbcd8bf
Wilfred <wilfred05777@gmail.com>
2022-12-26 11:59:21 +0800

    1:06:00 subTotal ui with out persist or local yet

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import Image from "next/legacy/image";
 import React from "react";
 import styles from "../styles/Navbar.module.css";
 import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
   const quantity = useSelector((state) => state.cart.quantity);
@@ -35,13 +36,15 @@ const Navbar = () => {
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" height={30} width={30} alt="logo" />
-          <div className={styles.counter}>{quantity}</div>
-          {/* <div className={styles.counter}>2</div> */}
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" height={30} width={30} alt="logo" />
+            <div className={styles.counter}>{quantity}</div>
+            {/* <div className={styles.counter}>2</div> */}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
